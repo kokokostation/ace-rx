a373eb368ee0366478df7b95fc76ce324b2f22c9
Your Name <you@example.com>
2023-02-21 19:50:00 +0100

    Revert "proxy all readonly endpoints through nginx"
    This reverts commit 0809346d8c3a93009c73f2f2e5d389ffa9cef68f.

--- src/config.js
+++ src/config.js
@@ -1,11 +1,10 @@
 'use strict';
 
-export const CHAT_READ_ENDPOINT = 'https://tuzach.net/api/?app=chat';
-export const CHAT_WRITE_ENDPOINT = 'https://tuzach.in/api/?app=chat';
+export const CHAT_ENDPOINT = 'https://tuzach.in/api/?app=chat';
 export const MEMEFEED_ENDPOINT = 'wss://memes-ace3.rhcloud.com:8443';
 // export const MEMEFEED_ENDPOINT = 'ws://localhost:3000';
 export const PLAYLIST_ENDPOINT = 'https://tuzach.in/api/?app=playlist';
-export const TOPIC_ENDPOINT = 'https://tuzach.net/api/?app=topic';
-export const CONTROL_ENDPOINT = 'https://tuzach.net/api/?app=admin';
+export const TOPIC_ENDPOINT = 'https://tuzach.in/api/?app=topic';
+export const CONTROL_ENDPOINT = 'https://tuzach.in/api/?app=admin';
 export const AVATAR_ENDPOINT = 'https://tuzach.in/api/?app=avatar';
-export const LOG_ENDPOINT = 'https://tuzach.net/api/?app=logs';
+export const LOG_ENDPOINT = 'https://tuzach.in/api/?app=logs';
